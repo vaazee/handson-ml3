a870a37073b3033ad30ee8fdd8309f064e06b996
Vasanth Rajendran <vaazee@gmail.com>
2025-04-22 11:38:02 -0400

    sync

---
+++ vaazee-experiments/linear-model.py
@@ -0,0 +1,42 @@
+import sys
+from packaging import version
+
+import matplotlib.pyplot as plt
+
+
+plt.rc("font", size=12)
+plt.rc("axes", labelsize=14, titlesize=14)
+plt.rc("legend", fontsize=12)
+plt.rc("xtick", labelsize=10)
+plt.rc("ytick", labelsize=10)
+
+
+import numpy as np
+
+np.random.seed(42)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Download and prepare the data
+data_root = "https://github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+X = lifesat[["GDP per capita (USD)"]].values
+y = lifesat[["Life satisfaction"]].values
+
+# Visualize the data
+lifesat.plot(kind="scatter", grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+# Select a linear model
+model = LinearRegression()
+
+# Train the model
+model.fit(X, y)
+
+# Make a prediction for Cyprus
+X_new = [[37_655.2]]  # Cyprus' GDP per capita in 2020
+print(model.predict(X_new))  # outputs [[6.30165767]]
